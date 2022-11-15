36efbfa1a26e4c6fb635bf215ce3573d690dad56
siki2-uiuc <siki2@illinois.edu>
2022-11-15 17:39:34 +0000

    Modernized controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,8 +24,8 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new
-    @movie.title = params.fetch("title")
-    @movie.description = params.fetch("description")
+    @movie.title = params.fetch(:title)
+    @movie.description = params.fetch(:description)
 
     if @movie.valid?
       @movie.save
@@ -44,8 +44,8 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params.fetch(:id))
 
-    @movie.title = params.fetch("title")
-    @movie.description = params.fetch("description")
+    @movie.title = params.fetch(:title)
+    @movie.description = params.fetch(:description)
 
     if @movie.valid?
       @movie.save
